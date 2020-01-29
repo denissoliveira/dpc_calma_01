c790820d8c940b0ba737cc2985ca01ea4ef7fea6
denis.oliveira <denis.oliveira@fasolti.com.br>
2020-01-29 17:10:48 -0300

    refactor(DB): Configuração conexão oracle

--- api/config/database.php
+++ api/config/database.php
@@ -1,17 +1,39 @@
 <?php
 class Database{
+
+    private $server         = "127.0.0.1";
+    private $db_username    = "SYSTEM";
+    private $db_password    = "Oracle_1";
+    private $service_name   = "ORCL";
+    private $sid            = "ORCL";
+    private $port           = 1522;
+    private $mydb="
+        (DESCRIPTION =
+            (ADDRESS = (PROTOCOL = TCP)(HOST = $server)(PORT = $port))
+            (CONNECT_DATA =
+                (SERVICE_NAME = $service_name)
+                (SID = $sid)
+            )
+        )";
+
+    private $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_NUM,
+    ];
  
-    private $host = "localhost";
-    private $db_name = "grudb";
-    private $username = "postgres";
-    private $password = "postgres";
+    // private $host = "localhost";
+    // private $db_name = "grudb";
+    // private $username = "postgres";
+    // private $password = "postgres";
     public $conn;
  
     public function getConnection(){
          $this->conn = null;
          try{
-            $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->exec("set names utf8");
+            $this-> conn = new PDO("oci:dbname=" . $mydb . ";charset=utf8", $db_username, $db_password, $opt)
+            //$this-> conn = new PDO("oci:dbname=".$mydb, $username, $password, $opt);
+            // $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            // $this->conn->exec("set names utf8");
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
         }
